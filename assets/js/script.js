956d177254c1cd5faf8d037f7a700c331c7595c5
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-17 11:30:09 -0700

    Added more functionality to dynamically changing time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,28 @@ setInterval(displayTime, 1000);
 // Function to color code the time blocks
 function updateColorCode() {
     let currHour = moment(dateObj).format("HH");
-    console.log(currHour);
+    // console.log(currHour);
+    //get all input-group elements
+    let inputGroupDivs = $(".input-group");
+    //loop through each input group div
+    for (let i = 0; i < inputGroupDivs.length; i++) {
+        //retrieve their id attribute
+        let divID = inputGroupDivs[i].attr('id');
+        console.log(divID);
+        if (currHour > divID) {
+            inputGroupDivs[i].childNodes;
+            console.log(inputGroupDivs[i].classList);
+        }
+    }
+
+    //if currHour > element.id
+    //add past class
+    //if currHour < element.id
+    //add future class
+    //if currHour == element.id
+    // present class
 }
 
 updateColorCode();
+
+//need a way to clear
