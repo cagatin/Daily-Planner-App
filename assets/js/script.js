ec84c1364074e4ea45d4cca0c34e1f95db72e19b
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-19 09:08:51 -0700

    updated functions to utilize jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,17 @@
 let dateObj;
 let date;
 
+//Variable to hold the save button 
+let saveBtn = $('.saveBtn')
+
+//Variable that refrences the current day paragraph
+let currDayPara = $('#currentDay');
+
 // Function to display time in the jumbotron
 function displayTime() {
     dateObj = new Date();
     date = moment(dateObj).format("MMMM Do, YYYY hh:mm:ss");
-    document.querySelector('#currentDay').textContent = date;
+    currDayPara.text(date);
 }
 
 //Display the time and update the time every second
@@ -19,36 +25,38 @@ function updateColorCode() {
     let currHour = moment(dateObj).format("HH");
 
     //get all input-group elements
-    let inputGroupDivs = document.querySelectorAll(".input-group")
+    let inputGroupDivs = $(".input-group")
 
     //loop through each input group div
     for (let i = 0; i < inputGroupDivs.length; i++) {
         //retrieve their id attribute
-        let divID = inputGroupDivs[i].getAttribute('id');
+        let divID = $(inputGroupDivs[i]).attr('id');
         console.log(divID);
 
         //From the array of children, select the input div
-        let inputElement = inputGroupDivs[i].children[1];
+        let inputElement = $(inputGroupDivs[i]).children().eq(1);
 
         //If the current hour is less than the ID... Its in the past!
         if (currHour > divID) {
-            if (inputElement.classList.contains('present')) {
-                inputElement.classList.remove('present');
+            if ($(inputElement).hasClass('present')) {
+                $(inputElement).removeClass('present');
             }
-            inputElement.classList.add('past');
+            $(inputElement).addClass('past');
         }
         //If the current hour is equal to the ID... Its the present!
         else if (currHour == divID) {
-            if (inputElement.classList.contains('future')) {
-                inputElement.classList.remove('future');
+            if ($(inputElement).hasClass('future')) {
+                $(inputElement).removeClass('future');
             }
-            inputElement.classList.add('present');
+            $(inputElement).addClass('present');
         }
         //Otherwise, its in the future!
         else {
-            inputElement.classList.add('future');
+            $(inputElement).addClass('future');
         }
     }
 }
+
+// Function to
 
 updateColorCode();
